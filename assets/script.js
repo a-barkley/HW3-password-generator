f2043aa651bc644fe5345eb273b046653a0a0e49
pbarkley_desk <rpbarkley99@gmail.com>
2022-06-24 17:13:08 -0500

    replace multiplication with if statements

--- assets/script.js
+++ assets/script.js
@@ -7,24 +7,39 @@ var allUppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
 var allNumbers = "0123456789";
 var allSpecial = "!\u0022#$%&\u0027()*+,-./:;<=>?@[\]^_`{|}~";
 var newPassword = "";
-
+var allCharacters = "";
 function generatePassword() {
   // Generates confirm and prompt alerts asking the user for password criteria
   var confirmLowercase = confirm("Would you like your password to include lowercase letters?");
+  if (confirmLowercase) {
+    allCharacters = allCharacters + allLowercase;
+  }
+
   var confirmUppercase = confirm("Would you like your password to include uppercase letters?");
+  if (confirmUppercase) {
+    allCharacters = allCharacters + allUppercase;
+  }
+
   var confirmNumbers = confirm("Would you like your password to include numbers?");
+  if (confirmNumbers) {
+    allCharacters = allCharacters + allNumbers;
+  }
+
   var confirmSpecial = confirm("Would you like your password to include special characters?");
+  if (confirmSpecial) {
+    allCharacters = allCharacters + allSpecial;
+  }
+
   var passLength = prompt("Choose a password length between 8 and 128");
   if (passLength < 8 || passLength > 128) {
     alert("Password must be between 8 characters and 128 characters");
   }
 
-  var allCharacters = (allLowercase * confirmLowercase) + (allUppercase * confirmUppercase) + (allNumbers * confirmNumbers) + (allSpecial * confirmSpecial);
-
   for (var i = 0; i <= passLength; i++) {
     var randomNumber = Math.floor(Math.random() * allCharacters.length);
     newPassword += allCharacters.substring(randomNumber, randomNumber + 1);
   }
+  console.log(newPassword);
   return newPassword;
 }
 
