9f4d5bbd987d16f96393f3c5b880dc1cec510823
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-06-24 11:12:16 -0500

    add strings with available characters

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generates confirm and prompt alerts asking the user for password criteria
 function generatePassword() {
-  var lowercase = confirm("Would you like your password to include lowercase letters?")
-  var uppercase = confirm("Would you like your password to include uppercase letters?")
-  var numbers = confirm("Would you like your password to include numbers?")
-  var special = confirm("Would you like your password to include special characters?")
+  var confirmLowercase = confirm("Would you like your password to include lowercase letters?")
+  var confirmUppercase = confirm("Would you like your password to include uppercase letters?")
+  var confirmNumbers = confirm("Would you like your password to include numbers?")
+  var confirmSpecial = confirm("Would you like your password to include special characters?")
   var passLength = prompt("Choose a password length between 8 and 128")
   if (passLength < 8 || passLength > 128) {
     alert("Password must be between 8 characters and 128 characters")
   }
-  
+  var allNums = "0123456789"
+  var allLowercase = "qwertyuiopasdfghjklzxcvbnm"
+  var allUppercase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+  var allSpecial = "!\u0022#$%&\u0027()*+,-./:;<=>?@[\]^_`{|}~"
 }
 
 // Write password to the #password input
