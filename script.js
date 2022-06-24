ccb96704095eb71d3733e29cf5a620a2c8aaa639
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-06-24 11:47:00 -0500

    add random number gen and substring selector

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Generates confirm and prompt alerts asking the user for password criteria
+
+var allLowercase = "qwertyuiopasdfghjklzxcvbnm"
+var allUppercase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+var allNumbers = "0123456789"
+var allSpecial = "!\u0022#$%&\u0027()*+,-./:;<=>?@[\]^_`{|}~"
+var newPassword = ""
+
 function generatePassword() {
-  var confirmLowercase = confirm("Would you like your password to include lowercase letters?")
-  var confirmUppercase = confirm("Would you like your password to include uppercase letters?")
-  var confirmNumbers = confirm("Would you like your password to include numbers?")
-  var confirmSpecial = confirm("Would you like your password to include special characters?")
-  var passLength = prompt("Choose a password length between 8 and 128")
+  // Generates confirm and prompt alerts asking the user for password criteria
+  var confirmLowercase = confirm("Would you like your password to include lowercase letters?");
+  var confirmUppercase = confirm("Would you like your password to include uppercase letters?");
+  var confirmNumbers = confirm("Would you like your password to include numbers?");
+  var confirmSpecial = confirm("Would you like your password to include special characters?");
+  var passLength = prompt("Choose a password length between 8 and 128");
   if (passLength < 8 || passLength > 128) {
-    alert("Password must be between 8 characters and 128 characters")
+    alert("Password must be between 8 characters and 128 characters");
   }
-  var allNums = "0123456789"
-  var allLowercase = "qwertyuiopasdfghjklzxcvbnm"
-  var allUppercase = "QWERTYUIOPASDFGHJKLZXCVBNM"
-  var allSpecial = "!\u0022#$%&\u0027()*+,-./:;<=>?@[\]^_`{|}~"
+
+  var allCharacters = (allLowercase * confirmLowercase) + (allUppercase * confirmUppercase) + (allNumbers * confirmNumbers) + (allSpecial * confirmSpecial);
+
+  for (var i = 0; i <= passLength; i++) {
+    var randomNumber = Math.floor(Math.random() * allCharacters.length);
+    newPassword += allCharacters.substring(randomNumber, randomNumber + 1);
+  }
+  return newPassword;
 }
 
 // Write password to the #password input
