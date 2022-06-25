93db840eb589e852eff9b6e38fef7b2ff43b25be
pbarkley_desk <rpbarkley99@gmail.com>
2022-06-24 21:48:43 -0500

    add final check to see if user selected at least one character type

--- assets/script.js
+++ assets/script.js
@@ -29,12 +29,18 @@ function generatePassword() {
   if (confirmSpecial) {
     allCharacters += allSpecial;
   }
+
   var passLength = prompt("Choose a password length between 8 and 128");
   // Checks if the user chose a valid number and throws up an alert if not
   if (passLength < 8 || passLength > 128) {
     alert("Password must be between 8 characters and 128 characters");
   }
 
+  if (confirmLowercase == false && confirmUppercase == false && confirmNumbers == false && confirmSpecial == false) {
+    alert("Must choose at least one character type!")
+  }
+
+  
   // Loops until the correct length chosen in passLength has been reached; cntinues randomly selecting characters until the password has reached the desired length
   for (var i = 0; i <= passLength; i++) {
     var randomNumber = Math.floor(Math.random() * allCharacters.length);
